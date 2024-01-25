f862d080dbb69cce3e790376f94c2b58e9fcbeca
ciko77 <153067882+ciko77@users.noreply.github.com>
2024-01-25 15:04:09 -0800

    end

--- homewrok1/script.js
+++ homewrok1/script.js
@@ -1,4 +1,6 @@
-let Name = prompt("");
-let Surname = prompt("");
-let dateOfBirth = prompt("");
-let Address = prompt("");
+let Name = prompt("შეიყვანეთ თქვენი სახელი");
+let Surname = prompt("შეიყვანეთ თქვენი გვარი");
+let dateOfBirth = prompt("შეიყვანეთ თქვენი დაბადების თარიღი");
+let Address = prompt("შეიყვანეთ თქვენი საცხოვრებელი ადგილი");
+let age = 2024 - dateOfBirth;
+alert("გამარჯობათ, " + Name + " " + Surname + " " +  ", მიხარია რომ უკვე" + dateOfBirth + " წლის ხარ და" + Address +  ცხოვრობ);
