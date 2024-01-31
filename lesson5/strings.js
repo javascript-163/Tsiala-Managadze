2c3ddd6b3b9aa13fa5da1d3a7042196e9013dafe
ciko77 <153067882+ciko77@users.noreply.github.com>
2024-01-31 09:47:15 -0800

    lesson4

---
+++ lesson5/strings.js
@@ -0,0 +1,67 @@
+let randomString = "Digital edu";
+console.log(randomString.charAt(4)); 
+console.log(randomString.at(4));
+console.log(randomString[4]);
+
+console.log(randomString.length);
+
+console.log(randomString.slice(0,8));
+
+console.log(randomString.slice(-10, -5))
+// console.log(randomString.substr(2, 5))   gamova gital
+
+// console.log(randomString.toUpperCase()); didi asoebi gamovida
+
+// console.log(randomString.concat(" ", "cation"))  konkatinacia
+
+// console.log(randomString.startsWith("d")); segidzlia aigo didi asoa tu patara
+// console.log(randomString.endsWith("D")); igivea bolo asoebis
+
+// console.log(randomString.replaceAll("i", "B")); poulobs arsebul asos addEventListener da anacvlebs marjvenit
+
+
+
+// let children = "Gaga, Gia, Tamari, Nutsa, Nini, Nika";
+
+// console.log(children.split(,))
+
+// striqonuli tipi gadayavs masivad
+
+
+
+// let children = "Gaga, Gia, Tamari, Nutsa, Nini, Nika";
+
+// let childrenArray = children.split(", ");
+// console.log(childrenArray.join("- "))
+// piriqit masivi gadaaketa striqonad
+
+// console.log(childrenArray.includes("Gaga"));
+
+
+
+// console.log(children.indexOf("N"));
+// awvdi da gibrunebs indexss
+
+let children = "Gaga, Gia, Tamari, Nutsa, Nini, Nika";
+
+let childrenArray = children.split(", ");
+
+let clonedChildren = Array.from(childrenArray);
+clonedChildren.pop();
+console.log(childrenArray);
+console.log(clonedChildren);
+
+ganmarteba
+siRmiseuli kopireba klonireba (originali ar icvleba axali masivi iqmneba da masze axdens cvlilebas)
+
+
+let person = {
+    name: "George"
+};
+
+console.log(typeof childrenArray, typeof person, Array.isArray(childrenArray));
+
+
+ganmarteba
+gexmareba masivi tua masivi rom gaigo
+da araa obieqti
