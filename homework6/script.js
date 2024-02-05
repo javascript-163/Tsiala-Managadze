3d3254460f8293a60714be2340d3710a5cf438a0
ciko77 <153067882+ciko77@users.noreply.github.com>
2024-02-05 14:45:30 -0800

    2

--- homework6/script.js
+++ homework6/script.js
@@ -14,3 +14,51 @@ return LongestWord;
 let sentence = "ეს წინადადება შედგება რამდენიმე სიტყვისაგან, ჩვენ შევეცდებით ვიპოვოთ ყველაზე გრძელი სიტყვა";
 let longest = findLongestWord(sentence);
 console.log("ყველაზე გრძელი სიტყვაა : " + longest);
+
+
+// 2. შექმენით ობიექტი სახელით "calculator" შესაბამისი ატრიბუტებით სახელად. num1, num2, ანუ რაღაც ესეთი {num1: 10, num2: 5}, დაამატეთ ამ ობიექტში ახალი ატრიბუტები სახელით add, substract, multiply, divide, მათი მთავარი მიზანი უნდა იქნეს, რომ გამოიანგარიშოს ამ ორი რიცხვის ჯამი, სხვაობა, ნამრავლი, განაყოფი. ეს ყველაფერი მოახდინეთ this საკვანძო სიტყვის მეშვეობით.
+
+let calculator = {
+     num1: 0,
+     nom2: 0,
+     setNumbers: function(num1, num2){
+        this.num1 = num1;
+        this.num2 = num2;
+     },
+     add: function() {
+        return this.num1 + this.num2;
+     },
+     subtract: function() {
+        return this.num1 - this.num2;
+     }
+     multiply: function() {
+        return this.num1 * this.num2;
+     }
+     divide: function() {
+        return this.num1 / this.num2;
+     }
+
+};
+calculator.setNumbers(10, 5);
+console.log("jami", calculator.add());
+console.log("sxvaoba", calculator.subtract());
+console.log("namravli", calculator.multiply());
+console.log("gayofa", calculator.divide());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
