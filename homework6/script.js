e1b0efc3aa98e61ad5407afe5bd5aa528b9aa4cc
ciko77 <153067882+ciko77@users.noreply.github.com>
2024-02-06 12:31:46 -0800

    dasrulebuli

--- homework6/script.js
+++ homework6/script.js
@@ -18,7 +18,7 @@ console.log("ყველაზე გრძელი სიტყვაა : " 
 
 // 2. შექმენით ობიექტი სახელით "calculator" შესაბამისი ატრიბუტებით სახელად. num1, num2, ანუ რაღაც ესეთი {num1: 10, num2: 5}, დაამატეთ ამ ობიექტში ახალი ატრიბუტები სახელით add, substract, multiply, divide, მათი მთავარი მიზანი უნდა იქნეს, რომ გამოიანგარიშოს ამ ორი რიცხვის ჯამი, სხვაობა, ნამრავლი, განაყოფი. ეს ყველაფერი მოახდინეთ this საკვანძო სიტყვის მეშვეობით.
 
-let calculator = {
+ let calculator = {
      num1: 0,
      nom2: 0,
      setNumbers: function(num1, num2){
@@ -30,10 +30,10 @@ let calculator = {
      },
      subtract: function() {
         return this.num1 - this.num2;
-     }
+     },
      multiply: function() {
         return this.num1 * this.num2;
-     }
+     },
      divide: function() {
         return this.num1 / this.num2;
      }
@@ -43,19 +43,21 @@ calculator.setNumbers(10, 5);
 console.log("jami", calculator.add());
 console.log("sxvaoba", calculator.subtract());
 console.log("namravli", calculator.multiply());
-console.log("gayofa", calculator.divide());
+console.log("ganayofi", calculator.divide());
+
+console.log("namravli", multiply(5, 3));
 
 
 
+// 3. შექმენით ფუნქცია, რომელიც შეიძლება გამოყენებულ იქნეს როგორც ფუნქციის ჰოისტინგის მაგალითად. შექმენით ეს ფუნქცია მისი გამოძახების შემდეგ და აღწერეთ კომენტარებით თუ როგორ მუშაობს იგი ჰოისტინგის მეშვეობით.
 
+console.log("namravli:", multiply(6, 3));
 
+function multiply(a, b) {
+   return a * b;
+}
 
-
-
-
-
-
-
+//  ამ მაგალითით multiply ფუნქციას ვიძახებთ ფაქტიურად მის კოდში გამოცხადებამდე. ჰოისტინგის მეშვეობით გამოცხდებული ფუნქცია ინაცვლებს ზემოთ და გამოცხადების დროს multiply უკვე შესრულებულია და ხელმისაწვდომი
 
 
 
