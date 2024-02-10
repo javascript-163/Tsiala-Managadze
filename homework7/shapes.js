d2e5c73ffdceb75b9a5617d545abf6fb6a5c24ae
ciko77 <153067882+ciko77@users.noreply.github.com>
2024-02-10 12:17:27 -0800

    Need to check

--- homework7/shapes.js
+++ homework7/shapes.js
@@ -12,11 +12,42 @@ class Shape {
     constructor(radius, name) {
         super();
     this.radius = radius;
-    this.name = name;
-
+   
 
     }
+
   area() {
-    return Math.PI * this.radius ** 2;
+    return Math.PI * this.radius * this.radius;
+  }
+} 
+class Rectangle extends Shape {
+  constructor(name, width, height){
+    super();
+    this.width = width;
+    this.height = height;
+
+  }
+  area() {
+    return this.width * this.height;
   }
 }
+class Triangle extends Shape {
+  constructor(name, base, height) {
+    super();
+    this.base = base;
+    this.height = height;
+
+  }
+
+  area() {
+  return (this.base * this.height) / 2;
+}
+}
+// vqmnit obieqtebs titoeuli figuristvis
+let circle = new Circle("Circle", 5);
+let rectangle = new Rectangle("Rectangle", 4, 6);
+let triangle = new Triangle("Triangle", 3, 4);
+
+console.log("Area of Circle: " + circle.area());
+console.log("Area of Rectangle: " + rectangle.area());
+console.log("Area of Triangle: " + triangle.area());
