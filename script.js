04c121dbc518bf77843cd70e5a3efd82ba976649
ciko77 <153067882+ciko77@users.noreply.github.com>
2024-02-13 07:05:49 -0800

    add change

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-Math.pow ("2", "2");
-2**2
-Math.pow (2, 2);
+let result = Math.pow (5, 2);
+console.log(result);
+
+// let result = 5 ** 2;
+// console.log(result)
 
 function celsiusToFahrenheit(celsius){
     let fahrenheit = (celsius * 9/5) + 32;
